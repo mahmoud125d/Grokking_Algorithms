e3267d92fbd764551c2b88020ebc743134848b14
mhmoud <mhmoudsalah1997@gmail.com>
2024-09-29 02:54:13 +0300

    first

---
+++ src/SelectionSort.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class SelectionSort {
+
+    public static void sort(int[] arr){
+        int temp;
+
+        for(int i=0;i<arr.length-1;i++){
+            int minIndex=i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j]<arr[minIndex]){
+                    minIndex =j;
+                }
+            }
+            temp=arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i]=temp;
+
+        }
+
+    }
+}
