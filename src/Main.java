a9068ee4d8125e808f9cfb9499aac511713c273a
mhmoud <mhmoudsalah1997@gmail.com>
2024-09-28 06:50:41 +0300

    first

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19};
+
+            System.out.println(BinarySearch.search(arr,13));
+
+
+    }
+}
